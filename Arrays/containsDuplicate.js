3a5ba3e441cf51930b819f8d4d5e0d079abe0490
Jenny Gao <gao.jenny9@gmail.com>
2020-11-16 22:37:46 +0000

    add containsDuplicate solution

---
+++ Arrays/containsDuplicate.js
@@ -0,0 +1,35 @@
+// Given an array of integers, find if the array contains any duplicates.
+
+// Your function should return true if any value appears at least twice in the array, and it should return false if every element is distinct.
+
+// Example 1:
+
+// Input: [1,2,3,1]
+// Output: true
+// Example 2:
+
+// Input: [1,2,3,4]
+// Output: false
+// Example 3:
+
+// Input: [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+const containsDuplicate = (nums) => {
+  if (nums.length === 0) {
+    return false;
+  }
+
+  nums.sort();
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === nums[i + 1]) {
+      return true;
+    }
+  }
+  return false;
+};
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([2, 14, 18, 22, 22]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
